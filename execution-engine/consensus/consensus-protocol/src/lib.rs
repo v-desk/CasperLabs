1752fb133f837a1247a93d251b5f66a20819763c
Mateusz Górski <gorski.mateusz@protonmail.ch>
2020-05-13 10:30:15 +0200

    HWY-24: Add an example implementation of the `DagSynchronizerState`.

--- execution-engine/consensus/consensus-protocol/src/lib.rs
+++ execution-engine/consensus/consensus-protocol/src/lib.rs
@@ -53,3 +53,77 @@ pub trait ConsensusProtocol<Ctx: ConsensusContext> {
         timer_id: TimerId,
     ) -> Result<ConsensusProtocolResult<Ctx>, anyhow::Error>;
 }
+
+#[cfg(test)]
+mod example {
+    use crate::{
+        protocol_state::{ProtocolState, Vertex},
+        synchronizer::DagSynchronizerState,
+        ConsensusContext, ConsensusProtocol, ConsensusProtocolResult, TimerId,
+    };
+    use anyhow::Error;
+
+    struct HighwayContext();
+
+    #[derive(Debug, Hash, PartialEq, Eq, Clone)]
+    struct VIdU64(u64);
+
+    #[derive(Debug, Hash, PartialEq, Eq, Clone)]
+    struct DummyVertex {
+        id: u64,
+        deploy_hash: DeployHash,
+    }
+
+    impl Vertex<DeployHash, VIdU64> for DummyVertex {
+        fn id(&self) -> VIdU64 {
+            VIdU64(self.id)
+        }
+
+        fn values(&self) -> Vec<DeployHash> {
+            vec![self.deploy_hash.clone()]
+        }
+    }
+
+    #[derive(Debug, Hash, PartialEq, Eq, Clone)]
+    struct DeployHash(u64);
+
+    impl ConsensusContext for HighwayContext {
+        type IncomingMessage = HighwayIncomingMessage;
+        type OutgoingMessage = HighwayOutgoingMessage;
+        type ConsensusValue = DeployHash;
+    }
+
+    enum HighwayIncomingMessage {
+        NewVertex(DummyVertex),
+        RequestVertex(VIdU64),
+    }
+
+    enum HighwayOutgoingMessage {}
+
+    impl<P: ProtocolState<VertexId = VIdU64, Vertex = DummyVertex>>
+        ConsensusProtocol<HighwayContext>
+        for DagSynchronizerState<VIdU64, DummyVertex, DeployHash, P>
+    {
+        fn handle_message(
+            &self,
+            msg: <HighwayContext as ConsensusContext>::IncomingMessage,
+        ) -> Result<ConsensusProtocolResult<HighwayContext>, Error> {
+            match msg {
+                HighwayIncomingMessage::RequestVertex(v_id) => unimplemented!(),
+                HighwayIncomingMessage::NewVertex(vertex) => unimplemented!(),
+            }
+        }
+
+        fn handle_timer(
+            &self,
+            timer_id: TimerId,
+        ) -> Result<ConsensusProtocolResult<HighwayContext>, Error> {
+            unimplemented!()
+        }
+    }
+
+    #[test]
+    fn foo() {
+        assert!(true)
+    }
+}
